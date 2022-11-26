aa2a78212eac56fe54de47d3df58255e85888aec
blazej-smorawski <blazej.smorawski@gmail.com>
2022-11-26 15:57:14 +0100

    Add UI app

---
+++ WindowsPackageManagerUI/Activation/IActivationHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace WindowsPackageManagerUI.Activation;
+
+public interface IActivationHandler
+{
+    bool CanHandle(object args);
+
+    Task HandleAsync(object args);
+}
---
+++ WindowsPackageManagerUI/Contracts/ViewModels/INavigationAware.cs
@@ -0,0 +1,8 @@
+﻿namespace WindowsPackageManagerUI.Contracts.ViewModels;
+
+public interface INavigationAware
+{
+    void OnNavigatedTo(object parameter);
+
+    void OnNavigatedFrom();
+}
