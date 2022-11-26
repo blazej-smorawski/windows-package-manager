4e75a46a3b7a34d3ccd191d6e7e01bdb45dc2cbb
blazej-smorawski <blazej.smorawski@gmail.com>
2022-11-26 19:11:10 +0100

    Testing data presistence

--- WindowsPackageManagerService/WindowsPackageManagerService.cs
+++ WindowsPackageManagerService/WindowsPackageManagerService.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading;
@@ -15,8 +20,11 @@ namespace WindowsPackageManagerService
 {
     public partial class WindowsPackageManagerService : ServiceBase
     {
+        private SortedDictionary<string, SortedSet<string>> logsCollection = new SortedDictionary<string, SortedSet<string>>();
         private static ManualResetEvent stopEvent = new ManualResetEvent(false);
         private static Thread loggingThread;
+        private static Thread persistingThread;
+        private string logFile = "C:\\WindowsPackageManager\\WindowsPackageManagerLog.txt";
 
 
         // ---------======= Extern code =======--------- //
@@ -49,9 +57,13 @@ namespace WindowsPackageManagerService
             eventLog.WriteEntry("In OnStart.");
             StartWindowsPackageManagerLogging();
 
-            loggingThread = new Thread(() => LoggingLoop(eventLog));
+            loggingThread = new Thread(LoggingLoop);
             loggingThread.IsBackground = true;
             loggingThread.Start();
+
+            persistingThread = new Thread(PersistLoop);
+            persistingThread.IsBackground = true;
+            persistingThread.Start();
         }
 
         protected override void OnStop()
@@ -64,17 +76,55 @@ namespace WindowsPackageManagerService
                 loggingThread.Abort();
             }
 
+            if (!persistingThread.Join(10000))
+            {
+                persistingThread.Abort();
+            }
+
             StopWindowsPackageManagerLogging();
         }
 
-        private static void LoggingLoop(EventLog eventLog)
+        private void LoggingLoop()
         {
             int timeOut = 1;
             while (!stopEvent.WaitOne(timeOut))
             {
-                StringBuilder log = new StringBuilder(2048);
-                int result = GetWindowsPackageManagerLog(log, 2048);
-                eventLog.WriteEntry("Log("+ result.ToString("X")+ "):"+log.ToString());
+                StringBuilder logBuilder = new StringBuilder(2048);
+                int result = GetWindowsPackageManagerLog(logBuilder, 2048);
+
+                string log = logBuilder.ToString();
+                eventLog.WriteEntry("Log("+ result.ToString("X")+ "):"+log);
+
+                string[] strings = log.Split(':');
+                eventLog.WriteEntry(strings[0] + " " + strings[1] + " " + strings[2]);
+
+                SortedSet<string> files;
+                if (logsCollection.TryGetValue(strings[0], out files))
+                {
+                    files.Add(strings[2]);
+                } 
+                else
+                {
+                    files = new SortedSet<string>();
+                    logsCollection.Add(strings[0], files);
+                }
+            }
+        }
+
+        private void PersistLoop()
+        {
+            int timeOut = 1000;
+            eventLog.WriteEntry("PersistLoop");
+            while (!stopEvent.WaitOne(timeOut))
+            {
+                eventLog.WriteEntry("Writing `logsCollection` into: "+logFile);
+                Stream ms = File.OpenWrite(logFile);
+
+                BinaryFormatter formatter = new BinaryFormatter();  
+                formatter.Serialize(ms, logsCollection);
+                ms.Flush();
+                ms.Close();
+                ms.Dispose();
             }
         }
     }
--- WindowsPackageManagerUI/Views/MainPage.xaml.cs
+++ WindowsPackageManagerUI/Views/MainPage.xaml.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.UI.Xaml.Controls;
-
+﻿using System.Collections.ObjectModel;
+using System.Runtime.Serialization.Formatters.Binary;
+using Microsoft.UI.Xaml.Controls;
+using WindowsPackageManagerUI.Helpers;
 using WindowsPackageManagerUI.ViewModels;
 
 namespace WindowsPackageManagerUI.Views;
 
 public sealed partial class MainPage : Page
 {
+    private ObservableCollection<TreeNode> DataSource = new ObservableCollection<TreeNode>();
+    private SortedDictionary<string, SortedSet<string>> logsCollection = new SortedDictionary<string, SortedSet<string>>();
+    private string logFile = "C:\\WindowsPackageManager\\WindowsPackageManagerLog.txt";
+
     public MainViewModel ViewModel
     {
         get;
@@ -15,5 +21,54 @@ public sealed partial class MainPage : Page
     {
         ViewModel = App.GetService<MainViewModel>();
         InitializeComponent();
+        DataSource = GetData();
+    }
+
+    private ObservableCollection<TreeNode> GetData()
+    {
+        //Format the object as Binary  
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        FileStream fs = File.Open(logFile, FileMode.Open);
+        logsCollection = (SortedDictionary<string, SortedSet<string>>)formatter.Deserialize(fs);
+        fs.Flush();
+        fs.Close();
+        fs.Dispose();
+
+        ObservableCollection<TreeNode> data = DataSource;
+        data.Clear();
+
+        foreach(var log in logsCollection)
+        {
+            ObservableCollection<TreeNode> children = new ObservableCollection<TreeNode>(); 
+            foreach(string file in log.Value)
+            {
+                children.Add(new TreeNode
+                {
+                    Name = file,
+                    Children = new ObservableCollection<TreeNode>()
+                });
+            }
+            data.Add(new TreeNode
+            {
+                Name = log.Key,
+                Children = children,
+            });
+        }
+
+        return data;
+    }
+}
+public class TreeNode
+{
+    public string Name
+    {
+        get; set;
+    }
+    public ObservableCollection<TreeNode> Children { get; set; } = new ObservableCollection<TreeNode>();
+
+    public override string ToString()
+    {
+        return Name;
     }
 }
